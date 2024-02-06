231691ca76a0971b35883cf8bf0511a7d33ee746
BYTESONNET <dhirajkr1117@gmail.com>
2024-02-06 05:31:55 +0530

    BUBBLE SORT

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+function BubbleSort(arr){
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr.length-i;j++){
+            if(arr[j]>arr[j+1]){
+                let d=arr[j]
+                arr[j]=arr[j+1]
+                arr[j+1]=d
+            }
+        }
+        
+
+    }
+    return arr
+}
+function main(){
+    let arr=[2,5,1,4,3]
+    console.log(BubbleSort(arr))
+}
+main()
